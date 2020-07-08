f8c85cb6d233a8cc9314403fce532215c2bbf9bc
slobodanmiric <slobamiric@gmail.com>
2020-07-08 07:55:22 +0200

    blog php zavrsen projekat

---
+++ 17-blog_php/index.php
@@ -0,0 +1,49 @@
+<?php include "partials/header.php"; ?>
+    
+    <!-- header -->
+    <header class="container">
+        <div class="col">
+            <h1>Dobrodosli  :)</h1>
+            <h3>Citajte nas i pratite na drustvenim mrezama</h3>
+        </div>
+        <div class="col">
+            <button type="button">Upoznaj nas&nbsp;<i class="fas fa-arrow-right"></i></button>
+            <button type="button">Poslji vest&nbsp;<i class="fas fa-paper-plane"></i></button>
+        </div>
+    </header>
+    
+    <!-- posts -->
+    <section class="container posts">
+        <article>
+            <img src="img/blog1.jpg" alt="">
+            <div class="text">
+                <h3>Blog jedan</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus praesentium, sit vero reprehenderit molestias officiis aliquid! Assumenda molestias cum, qui harum officia porro error quos nobis totam soluta. Esse, minus.</p>
+                <button type="button">Procitaj ceo tekst</button>
+            </div>
+        </article>
+        <article>
+            <img src="img/blog2.jpg" alt="">
+            <div class="text">
+                <h3>Blog dva</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus praesentium, sit vero reprehenderit molestias officiis aliquid! Assumenda molestias cum, qui harum officia porro error quos nobis totam soluta. Esse, minus.</p>
+                <button type="button">Procitaj ceo tekst</button>
+            </div>
+        </article>
+        <article>
+            <img src="img/blog3.jpg" alt="">
+            <div class="text">
+                <h3>Blog tri</h3>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus praesentium, sit vero reprehenderit molestias officiis aliquid! Assumenda molestias cum, qui harum officia porro error quos nobis totam soluta. Esse, minus.</p>
+                <button type="button">Procitaj ceo tekst</button>
+            </div>
+        </article>
+    </section>
+    
+    
+<?php include "partials/footer.php"; ?>
+    
+    
